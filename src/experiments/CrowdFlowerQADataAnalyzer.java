38573d99c6a71ae3953b79ec721e9effca9e556d
Luheng He <luheng@uw.edu>
2015-02-25 21:27:50 -0800

    data analysis (heart)

--- src/experiments/CrowdFlowerQADataAnalyzer.java
+++ src/experiments/CrowdFlowerQADataAnalyzer.java
@@ -4,49 +4,105 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import util.Distribution;
+import util.StringUtils;
 import data.AnnotatedSentence;
 import data.SRLCorpus;
+import data.SRLSentence;
 import data.StructuredQAPair;
 import annotation.CrowdFlowerQAResult;
 
 public class CrowdFlowerQADataAnalyzer {
 
-	static void aggregateAnnotations(
-			ArrayList<AnnotatedSentence> annotatedSentences) {
-		for (AnnotatedSentence annotSent : annotatedSentences) {			
-			for (int propHead : annotSent.qaLists.keySet()) {
-				HashMap<String, Integer> aggregatedQAMap =
-						new HashMap<String, Integer>();
-				for (StructuredQAPair qa : annotSent.qaLists.get(propHead)) {
-					String encodedAnswer = qa.getAnswerString();
-					if (!aggregatedQAMap.containsKey(encodedAnswer)) {
-						aggregatedQAMap.put(encodedAnswer, 1);
-					} else {
-						int k = aggregatedQAMap.get(encodedAnswer);
-						aggregatedQAMap.put(encodedAnswer, k + 1);
-					}
+	private static SRLCorpus corpus;
+	private static ArrayList<CrowdFlowerQAResult> annotationResults;
+	private static ArrayList<AnnotatedSentence> annotatedSentences;
+	
+	private static void completionTimeAnalysis() {
+		double avgTime = 0, minTime = 1e8, maxTime = 0;
+		
+		for (CrowdFlowerQAResult result : annotationResults) {
+			double secs = result.secondsToComplete;
+			avgTime += secs;
+			minTime = Math.min(minTime, secs);
+			maxTime = Math.max(maxTime, secs);
+		}
+		
+		System.out.println("Averaged: " + avgTime / annotationResults.size() + "\n" +
+						   "Minimum: " + minTime + "\n" +
+						   "Maximum: " + maxTime);
+		
+		// TODO
+		// Distribution of completion time
+	}
+	
+	private static void workerAnalysis() {
+		HashMap<Integer, ArrayList<CrowdFlowerQAResult>> workerMap =
+				new HashMap<Integer, ArrayList<CrowdFlowerQAResult>>();
+		
+		for (CrowdFlowerQAResult result : annotationResults) {
+			int workerId = result.cfWorkerId;
+			if (!workerMap.containsKey(workerId)) {
+				workerMap.put(workerId, new ArrayList<CrowdFlowerQAResult>());
+			}
+			workerMap.get(workerId).add(result);
+		}
+		
+		// Analysis 1: completion time
+		//          2: number of QAs (per sentence)
+		System.out.println("Worker\tNum rows\tAvg time\tMin time\tMax time\tAvg num QAs");
+		
+		for (int workerId : workerMap.keySet()) {
+			ArrayList<CrowdFlowerQAResult> results = workerMap.get(workerId);
+			HashMap<Integer, Integer> qaCounter = new HashMap<Integer, Integer>();
+			Distribution compTime = new Distribution(),
+					     numQAs = new Distribution();
+			
+			for (CrowdFlowerQAResult result : results) {
+				
+				// Completion time.
+				compTime.add(result.secondsToComplete);
+				
+				// Number of QAs
+				int cnt = qaCounter.containsKey(result.sentenceId) ?
+						qaCounter.get(result.sentenceId) : 0;
+				qaCounter.put(result.sentenceId, cnt + 1);
+			}
+			
+			for (int sentId : qaCounter.keySet()) {
+				numQAs.add(qaCounter.get(sentId));
+			}
+			
+			System.out.println(String.format("%d\t%d\t%.3f\t%.3f\t%.3f\t%.3f",
+					workerId, compTime.getNumSamples(), compTime.getMean(),
+					compTime.getMin(), compTime.getMax(),
+					numQAs.getMean()));
+		}	
+	}
+	
+	private static void printFeedback() {
+		for (CrowdFlowerQAResult result : annotationResults) {
+			if (!result.feedback.isEmpty()) {
+				SRLSentence sentence = (SRLSentence)
+						corpus.sentences.get(result.sentenceId);
+				System.out.println("Proposition:\t" + result.proposition);
+				for (int i = 0; i < result.questions.size(); i++) {
+					System.out.println("\t" + StringUtils.join(" ", result.questions.get(i)));
+					System.out.println("\t\t" + StringUtils.join(" ", result.answers.get(i)));
 				}
-				ArrayList<StructuredQAPair> agreedList =
-						new ArrayList<StructuredQAPair>();
-				for (StructuredQAPair qa : annotSent.qaLists.get(propHead)) {
-					String encodedAnswer = qa.getAnswerString();
-					if (aggregatedQAMap.get(encodedAnswer) > 1) {
-						agreedList.add(qa);
-						aggregatedQAMap.put(encodedAnswer, 0);
-					}
-				}
-				annotSent.qaLists.put(propHead, agreedList);
+				System.out.println(sentence.toString());
+				System.out.println(result.feedback + "\n");
 			}
 		}
 	}
 	
+	//private static void 
+	
 	public static void main(String[] args) {
-		SRLCorpus trainCorpus = ExperimentUtils.loadSRLCorpus(
+		corpus = ExperimentUtils.loadSRLCorpus(
 				ExperimentUtils.conll2009TrainFilename, "en-srl-train");
-		ArrayList<CrowdFlowerQAResult> annotationResults =
-				new ArrayList<CrowdFlowerQAResult>();
-		ArrayList<AnnotatedSentence> annotatedSentences =
-				new ArrayList<AnnotatedSentence>();
+		annotationResults = new ArrayList<CrowdFlowerQAResult>();
+		annotatedSentences = new ArrayList<AnnotatedSentence>();
 		
 		try {
 			CrowdFlowerQADataRetriever.readAnnotationResult(annotationResults);
@@ -55,7 +111,11 @@ public class CrowdFlowerQADataAnalyzer {
 			return;
 		}
 		CrowdFlowerQADataRetriever.alignAnnotations(annotatedSentences,
-				annotationResults, trainCorpus);
+				annotationResults, corpus);
 		//aggregateAnnotations(annotatedSentences);
+		
+		//printFeedback();
+		//completionTimeAnalysis();
+		workerAnalysis();
 	}
 }
---
+++ src/util/Distribution.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.util.ArrayList;
+
+// TODO: print histogram and blah blah.
+
+public class Distribution {	
+	ArrayList<Double> values;
+	double sum, minValue, maxValue;
+	
+	public Distribution() {
+		values = new ArrayList<Double>();
+		sum = .0;
+	}
+	
+	public void clear() {
+		values.clear();
+		sum = 0;
+	}
+	
+	public void add(double val) {
+		minValue = values.isEmpty() ? val : Math.min(minValue, val);
+		maxValue = values.isEmpty() ? val : Math.max(maxValue, val);
+		sum += val;
+		values.add(val);
+	}
+	
+	public double getSum() {
+		return sum; 
+	}
+	
+	public double getMean() {
+		return sum / values.size();
+	}
+	
+	public double getMin() {
+		return minValue;
+	}
+	
+	public double getMax() {
+		return maxValue;
+	}
+	
+	public int getNumSamples() {
+		return values.size();
+	}
+}
