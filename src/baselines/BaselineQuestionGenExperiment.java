77de4594f13f894e46143defee3d9965e32c6f8f
Luheng He <luheng@portal2.dyn.cs.washington.edu>
2015-05-02 16:57:57 -0700

    question generation prototype

--- src/baselines/BaselineQuestionGenExperiment.java
+++ src/baselines/BaselineQuestionGenExperiment.java
@@ -1,5 +1,66 @@
 package baselines;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
+
+import config.DataConfig;
+import config.QuestionGenConfig;
+import util.StringUtils;
+import annotation.QuestionEncoder;
+import learning.KBestParseRetriever;
+import learning.QASample;
+import learning.QuestionIdDataset;
+import learning.QuestionIdFeatureExtractor;
+import data.Corpus;
+import data.CountDictionary;
+import data.QAPair;
+import experiments.LiblinearHyperParameters;
+
 public class BaselineQuestionGenExperiment {
 
+	private QuestionGenConfig config;
+	private Corpus baseCorpus; 
+	private QuestionIdFeatureExtractor featureExtractor;
+	
+	private QuestionIdDataset trainSet;
+	private HashMap<String, QuestionIdDataset> testSets;
+	
+	public BaselineQuestionGenExperiment() throws IOException {
+		config = new QuestionGenConfig();
+		baseCorpus = new Corpus("qa-exp-corpus");
+		testSets = new HashMap<String, QuestionIdDataset>();
+		
+		// ********** Load QA Data ********************
+		if (config.trainWithWiki) {
+			trainSet = new QuestionIdDataset(baseCorpus, "wiki1-train");
+			testSets.put("prop-train", new QuestionIdDataset(baseCorpus, "prop-train"));
+			trainSet.loadData(DataConfig.get("wikiQATrainFilename"));
+			testSets.get("prop-train").loadData(DataConfig.get("propbankQATrainFilename"));
+		} else {
+			trainSet = new QuestionIdDataset(baseCorpus, "prop-train");
+			testSets.put("wiki1-train", new QuestionIdDataset(baseCorpus, "wiki1-train"));
+			trainSet.loadData(DataConfig.get("propbankQATrainFilename"));
+			testSets.get("wiki1-train").loadData(DataConfig.get("wikiQATrainFilename"));
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		BaselineQuestionGenExperiment exp = null;
+		try {
+			exp = new BaselineQuestionGenExperiment();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	
+	}
 }
--- src/config/DataConfig.java
+++ src/config/DataConfig.java
@@ -5,36 +5,25 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class DataConfig {
-	public static Properties properties = null;
-	
-	public static void config() throws IOException {
+	static Properties properties = null;
+	static {
 		properties = new Properties();
-		FileInputStream in = new FileInputStream("dataConfig.properties");
-		properties.load(in);
-		in.close();
-		
-		for (Object key : properties.keySet()) {
-			System.out.println(key + ", " + properties.getProperty((String) key));
+		FileInputStream in;
+		try {
+			in = new FileInputStream("dataConfig.properties");
+			properties.load(in);
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-
+	
 	public static String get(String key) {
-		if (properties == null) {
-			try {
-				config();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 		return properties.getProperty(key).trim();
 	}
 	
 	public static void main(String[] args) {
-		try {
-			DataConfig.config();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	
 	}
 	
 }
