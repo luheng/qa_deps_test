e43b30f8deed891b1fe0a0af520dd8647b93d5cc
Luheng He <luheng@portal2.dyn.cs.washington.edu>
2015-05-04 01:22:58 -0700

    fwdbkwd

--- src/learning/QGenFactorGraph.java
+++ src/learning/QGenFactorGraph.java
@@ -1,5 +1,7 @@
 package learning;
 
+import util.LatticeHelper;
+
 public class QGenFactorGraph {
 	private static final int maxCliqueSize = 10000;
 	
@@ -8,11 +10,11 @@ public class QGenFactorGraph {
 	public double[][] cliqueScore;
 	public double[][] cliqueMarginal;
 	public double[][] stateMarginal;
-	public double[][][] alpha, beta, best;
+	public double[][] alpha, beta, best;
 	public int[][][] prev;
 	public int[] decode;
 	public double logNorm;
-	private double[] dpTemplate;
+	private double[] dp;
 	private int sequenceLength;
 	
 	public QGenFactorGraph(QGenSequence sequence,
@@ -21,14 +23,18 @@ public class QGenFactorGraph {
 		cliqueScore = new double[sequenceLength][];
 		cliqueMarginal = new double[sequenceLength][];
 		stateMarginal = new double[sequenceLength][];
+		alpha = new double[sequenceLength][];
+		beta = new double[sequenceLength][];
 		int[] csizes = potentialFunction.cliqueSizes;
 		int[] lsizes = potentialFunction.latticeSizes;
 		for (int i = 0; i < sequenceLength; i++) {
 			cliqueScore[i] = new double[csizes[i]];
 			cliqueMarginal[i] = new double[csizes[i]];
+			alpha[i] = new double[csizes[i]];
+			beta[i] = new double[csizes[i]];
 			stateMarginal[i] = new double[lsizes[i]];
 		}
-		dpTemplate = new double[maxCliqueSize];
+		dp = new double[maxCliqueSize];
 		decode = new int[sequenceLength];
 	}
 	
@@ -43,58 +49,67 @@ public class QGenFactorGraph {
 	}
 	
 	public void computeMarginals() {
-		for (int s : fiter.states(sequenceID, 0)) { 
-			alpha[0][s][S0] = edgeScore[0][s][S0][S00] + nodeScore[0][s];
+		int[][] iterator = potentialFunction.iterator;
+		for (int s = 0; s < iterator[0][0]; s++) {
+			int cliqueId = potentialFunction.getCliqueId(0, s, 0, 0);
+			alpha[0][s] = cliqueScore[0][cliqueId];
 		}
-		int len;
-		for (int i = 1; i <= length; i++) {
-			for (int s : fiter.states(sequenceID, i)) {
-				for (int sp : fiter.states(sequenceID, i-1)) {
+		int len = 0;
+		for (int i = 1; i < sequenceLength; i++) {
+			for (int s = 0; s < iterator[i][0]; s++) { 
+				for (int sp = 0; sp < iterator[i][1]; sp++) {
 					len = 0;
-					for (int spp : fiter.states(sequenceID, i-2)) {
-						dpTemplate[len++] = alpha[i-1][sp][spp] +
-								edgeScore[i][s][sp][spp] + nodeScore[i][s];
+					for (int spp = 0; spp < iterator[i][2]; spp++) {
+						int cLeft = spp * iterator[i][1] + sp;
+						int c = cLeft * iterator[i][0] + s;
+						dp[len++] = alpha[i-1][cLeft] + cliqueScore[i][c];
 					}
-					alpha[i][s][sp] = LatticeHelper.logsum(dpTemplate, len);
+					int cRight = sp * iterator[i][0] + s;
+					alpha[i][cRight] = LatticeHelper.logsum(dp, len);
 				}
 			}
 		}
 		len = 0;
-		for (int sp : fiter.states(sequenceID, length-1)) {
-			beta[length][SN][sp] = 0;
-			dpTemplate[len++] = alpha[length][SN][sp];
+		int N = sequenceLength - 1;
+		for (int sp = 0; sp < iterator[N][1]; sp++) {
+			for (int spp = 0; spp < iterator[N][2]; spp++) {
+				int cOld = spp * iterator[N][1] + sp;
+				beta[N][cOld] = 0;
+				dp[len++] = alpha[N][cOld];
+			}
 		}
-		logNorm = LatticeHelper.logsum(dpTemplate, len);
+		logNorm = LatticeHelper.logsum(dp, len);
 
-		for (int i = length; i > 0; i--) { 
-			for (int sp : fiter.states(sequenceID, i - 1)) { 
-				for (int spp : fiter.states(sequenceID, i - 2)) {
+		for (int i = sequenceLength - 1; i > 0; i--) {
+			for (int spp = 0; spp < iterator[i][2]; spp++) {
+				for (int sp = 0; sp < iterator[i][1]; sp++) {				
 					len = 0;
-					for (int s : fiter.states(sequenceID, i)) {
-						dpTemplate[len++] = beta[i][s][sp] +
-								edgeScore[i][s][sp][spp] + nodeScore[i][s];
+					for (int s = 0; s < iterator[i][0]; s++) {
+						int cRight = sp * iterator[i][0] + s;
+						int c = iterator[i][1] * spp + cRight;
+						dp[len++] = beta[i][cRight] + cliqueScore[i][c];
 					}
-					beta[i-1][sp][spp] = LatticeHelper.logsum(dpTemplate, len);
+					int cLeft = spp * iterator[i][1] + sp;
+					beta[i-1][cLeft] = LatticeHelper.logsum(dp, len);
 				}
 			}
 		}
-		for (int s : fiter.states(sequenceID, 0)) {
-			nodeMarginal[0][s] = edgeMarginal[0][s][S0][S00] =
-					alpha[0][s][S0] + beta[0][s][S0] - logNorm;
-		}
-		for (int i = 1; i <= length; i++) {
-			for (int s : fiter.states(sequenceID, i)) {
+		for (int i = 0; i < sequenceLength; i++) {
+			for (int s = 0; s < iterator[i][0]; s++) {
 				len = 0;
-				for (int sp : fiter.states(sequenceID, i - 1)) { 
-					for (int spp : fiter.states(sequenceID, i - 2)) {
-						edgeMarginal[i][s][sp][spp] = alpha[i-1][sp][spp] +
-								beta[i][s][sp] + edgeScore[i][s][sp][spp] +
-								nodeScore[i][s] - logNorm;
+				for (int sp = 0; sp < iterator[i][1]; sp++) {
+					int cRight = sp * iterator[i][0] + s;
+					for (int spp = 0; spp < iterator[i][2]; spp++) {
+						int cLeft = spp * iterator[i][1] + sp;
+						int c = cLeft * iterator[i][0] + s;
+						cliqueMarginal[i][c] =
+								alpha[i-1][cLeft] + beta[i][cRight] +
+								cliqueScore[i][c] - logNorm;
+						
 					}
-					dpTemplate[len++] = alpha[i][s][sp] + beta[i][s][sp] -
-							logNorm;
+					dp[len++] = alpha[i][cRight] + cliqueScore[i][cRight];
+					stateMarginal[i][s] = LatticeHelper.logsum(dp, len);
 				}
-				nodeMarginal[i][s] = LatticeHelper.logsum(dpTemplate, len);
 			}
 		}
 	}
