e9a67fb3ae6f96809a250be478b4823769549247
Luheng He <luheng@uw.edu>
2015-05-21 21:51:07 -0700

    tense

--- src/baselines/QuestionGenerator.java
+++ src/baselines/QuestionGenerator.java
@@ -97,58 +97,57 @@ public class QuestionGenerator {
 		String[] aux = getAuxiliary(sentence, propHead);
 		boolean isPassive = false, hasNegation = false;
 		String newStr = "";
-		if (aux != null) {
-			String auxStr = StrUtils.join(" ", aux);
-			System.out.println(sentence.getTokensString() + "\n" + auxStr + " " + sentence.getTokenString(propHead));
-			hasNegation = (auxStr.contains("\'t") || auxStr.contains("not"));
-			if (!verb.endsWith("ing") && (
-					auxStr.contains("been") || auxStr.contains("being") ||
-					auxStr.contains("be") || auxStr.contains("is") ||
-					auxStr.contains("are") || auxStr.contains("were") ||
-					auxStr.contains("was"))) {
-				isPassive = true;
+
+		String auxStr = (aux == null ? StrUtils.join(" ", aux) : "");
+		System.out.println(sentence.getTokensString() + "\n" + auxStr + " " + sentence.getTokenString(propHead));
+		hasNegation = (auxStr.contains("\'t") || auxStr.contains("not"));
+		if (!verb.endsWith("ing") && (
+				auxStr.contains("been") || auxStr.contains("being") ||
+				auxStr.contains("be") || auxStr.contains("is") ||
+				auxStr.contains("are") || auxStr.contains("were") ||
+				auxStr.contains("was"))) {
+			isPassive = true;
+		}
+		//System.out.println(hasNegation ? "Neg" : "");
+		//System.out.println(isPassive ? "Passive" : "Active");
+		if (isPassive) {
+			// make active
+			if (auxStr.contains("been")) {
+				newStr = auxStr.replaceAll("been", "") + " " + verb;
+			} else if (auxStr.contains("being")) {
+				newStr = auxStr.replaceAll("being", "") + " " + infl[2];
+			} else if (auxStr.contains("be ")) {
+				newStr = auxStr.replaceAll("be", "") + " " + infl[0];
+			} else if (auxStr.contains("is") || auxStr.contains("are")) {
+				newStr = infl[1];
+			} else if (auxStr.contains("were") || auxStr.contains("was")) {
+				newStr = infl[4];
 			}
-			//System.out.println(hasNegation ? "Neg" : "");
-			//System.out.println(isPassive ? "Passive" : "Active");
-			if (isPassive) {
-				// make active
-				if (auxStr.contains("been")) {
-					newStr = auxStr.replaceAll("been", "") + " " + verb;
-				} else if (auxStr.contains("being")) {
-					newStr = auxStr.replaceAll("being", "") + " " + infl[2];
-				} else if (auxStr.contains("be ")) {
-					newStr = auxStr.replaceAll("be", "") + " " + infl[0];
-				} else if (auxStr.contains("is") || auxStr.contains("are")) {
-					newStr = infl[1];
-				} else if (auxStr.contains("were") || auxStr.contains("was")) {
-					newStr = infl[4];
-				}
-				System.out.println("pas->act:\t" + newStr);
-			} else {
-				// make passive
-				if (verb.equals(infl[2])) {
-					newStr = auxStr + " being " + infl[4];
-				} else if (verb.equals(infl[4]) &&
-						(auxStr.contains("has") || auxStr.contains("have") ||
-						 auxStr.contains("had"))) {
-					newStr = auxStr + " been " + infl[4];
-				} else if (verb.equals(infl[1])) {
-					if (auxStr.isEmpty()) {
-						newStr = "is " + infl[4];
-					} else if (auxStr.contains("do ")) {
-						newStr = auxStr.replace("do", "is") + " " + infl[4];
-					} else if (auxStr.contains("does ")) {
-						newStr = auxStr.replace("does", "is") + " " + infl[4];
-					} else if (auxStr.contains("did ")) {
-						newStr = auxStr.replace("did", "was") + " " + infl[4];
-					} else {
-						newStr = auxStr + " be " + infl[4];
-					}
+			System.out.println("pas->act:\t" + newStr);
+		} else {
+			// make passive
+			if (verb.equals(infl[2])) {
+				newStr = auxStr + " being " + infl[4];
+			} else if (verb.equals(infl[4]) &&
+					(auxStr.contains("has") || auxStr.contains("have") ||
+					 auxStr.contains("had"))) {
+				newStr = auxStr + " been " + infl[4];
+			} else if (verb.equals(infl[1])) {
+				if (auxStr.isEmpty()) {
+					newStr = "is " + infl[4];
+				} else if (auxStr.contains("do ")) {
+					newStr = auxStr.replace("do", "is") + " " + infl[4];
+				} else if (auxStr.contains("does ")) {
+					newStr = auxStr.replace("does", "is") + " " + infl[4];
+				} else if (auxStr.contains("did ")) {
+					newStr = auxStr.replace("did", "was") + " " + infl[4];
 				} else {
-					newStr = "was " + infl[4];
+					newStr = auxStr + " be " + infl[4];
 				}
-				System.out.println("act->pas:\t" + newStr);
+			} else {
+				newStr = "was " + infl[4];
 			}
+			System.out.println("act->pas:\t" + newStr);
 		}
 		
 		ArrayList<String[]> questions = new ArrayList<String[]>();
